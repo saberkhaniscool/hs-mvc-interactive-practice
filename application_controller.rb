df643aaca8c853a2d7f721b154eb4617dd50b705
Saber Khan <skhan@browning.edu>
2017-08-02 15:14:21 +0000

    dog.rb

--- application_controller.rb
+++ application_controller.rb
@@ -1,10 +1,21 @@
 require 'bundler'
 Bundler.require
 
+require_relative './models/dog.rb'
+
 class MyApp < Sinatra::Base
 
   get '/' do
     erb :index
   end
+  
+  get '/dog' do
+    @dog1 = Dog.new("Fido", "Lab", "5")
+    erb :dog
+  end 
 
+  get '/info' do
+    erb :info
+  end
+  
 end
---
+++ models/dog.rb
@@ -0,0 +1,9 @@
+class Dog
+    attr_accessor :name, :breed, :age
+    
+    def initialize(name, breed, age)
+        @name = name
+        @age = age
+        @breed = breed
+    end
+end
